e9765b87b9ad1c8ce2d60c11e9391d1d19fc2c4d
이승준 <fate10503@gmail.com>
2022-12-24 22:28:39 +0900

    firebase login api

--- client/pages/signUp.js
+++ client/pages/signUp.js
@@ -1,42 +1,28 @@
 import { useRouter } from 'next/router';
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { firebase } from "../firebase";
 import { useState } from 'react';
 
 const auth = getAuth(firebase);
 
-const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const SignUp = () => {
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
   const router = useRouter();
 
-  const goToSignUp = () => {
-    router.push('/signUp');
-  };
-
-  const googleLogin = async() => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider).then((userData) => {
-    const credential = GoogleAuthProvider.credentialFromResult(userData);
-    const token = credential.accessToken;
-    localStorage.setItem('accessToken', token)
-    router.push('/');
+  const SignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      router.push('/signIn');
+      // ...
     })
-  }
-
-  const signIn = async() => {
-    try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    const token = await user.getIdToken()
-    localStorage.setItem('accessToken', token)
-    router.push('/');
-  
-    }  catch {
+    .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      window.alert('아이디 및 비밀번호가 틀립니다')
-    };
+      // ..
+    });
   };
 
   return (
@@ -44,13 +30,28 @@ const SignIn = () => {
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Sign up your account
           </h2>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" action="#" method="POST">
+            <div>
+                <label htmlFor="nickname" className="block text-sm font-medium text-gray-700">
+                  Nickname
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="nickname"
+                    name="nickname"
+                    type="nickname"
+                    autoComplete="nickname"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
@@ -84,42 +85,13 @@ const SignIn = () => {
                   />
                 </div>
               </div>
-
               <div>
                 <button
-                  onClick={signIn}
-                  type="button"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Sign in
-                </button>
-              </div>
-
-              <div>
-                <button
-                  onClick={goToSignUp}
+                  onClick={SignUp}
                   type="button"
                   className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                 >
                   Sign Up
-                </button>
-              </div>
-              <div>
-                <button
-                  onClick={googleLogin}
-                  type="button"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  GOOGLE LOGIN
-                </button>
-              </div>
-              <div>
-                <button
-                  onClick={() => alert('sign in')}
-                  type="button"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  FACEBOOK LOGIN
                 </button>
               </div>
             </form>
@@ -130,4 +102,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SignUp;
