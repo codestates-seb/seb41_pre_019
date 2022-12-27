0a2c0e1f6c8eed4296a36b7558e26b0ea8392b1c
YerimSW <swyr1016@gmail.com>
2022-12-27 17:56:38 +0900

    Feat API 계층 유효성검증 예외처리

---
+++ server/src/main/java/pre019/server/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,32 @@
+package pre019.server.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+// 각 컨트롤러에서 발생하는 에러에 대해 예외처리를 공통화 하기 위한 클래스.
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+       final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+       return response;
+    } // DTO 유효성 검증 예외처리
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+        return response;
+    } // PathVariable 유효성 검증 예외처리
+
+    // TODO 비즈니스 로직 예외처리
+}
