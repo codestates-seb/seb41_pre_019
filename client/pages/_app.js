52e620e0f74a0a0bb77873b22f7c0e7703db6f85
박상일 <skdisk5304@npixel.co.kr>
2022-12-28 11:55:52 +0900

    error fix

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,14 @@
-import Nav from '../components/Nav';
+import { Provider } from "react-redux";
 
-import '../styles/globals.css';
-
+import store from "../redux/index";
+import Nav from "../components/Nav";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Nav />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
-
-
