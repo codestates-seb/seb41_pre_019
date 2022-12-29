ead42cda8d6e8a253ab5f568f089c9271ee3facb
FE012 <twte96@gmail.com>
2022-12-29 21:07:26 +0900

    fix 해야함

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,8 +3,7 @@ import { Provider } from "react-redux";
 import store from "../redux/index";
 import Nav from "../components/Nav";
 import "../styles/globals.css";
-
-
+import QuestionContent from "./Questions/QuestionContent";
 
 export default function App({ Component, pageProps }) {
   return (
